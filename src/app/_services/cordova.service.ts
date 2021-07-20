70fb6ff673119c9236b0bd86f3d24983e995dbf1
Jonatan <jonatan.lledo@gmail.com>
2021-07-20 12:56:07 +0200

    Testing Cordova inside Angular and more tests done

---
+++ src/app/_services/cordova.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+declare var device: any;
+declare var cordova: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CordovaService {
+
+  // device: any = device;
+  // cordova: any = cordova;
+
+  constructor() { }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { CordovaService } from './_services';
+
+declare var device: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'testCordova';
+
+  // constructor ( private cordovaSv: CordovaService ) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    document.addEventListener('deviceready', () => {
+      console.log('%c testing cordova', 'color: deeppink', device.cordova);
+    } , false);
+  }
+
+  onClick() {
+    console.log('%c testing onClick cordova', 'color: deeppink', device.cordova);
+  }
+
 }
